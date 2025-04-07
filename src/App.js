a33d3243cb32eb1418dd9f60f4da57cd0a238f6e
Can Karahan <cankarahan200361@hotmail.com>
2025-04-07 21:51:27 +0300

    frontend içeriği eklendi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/")
-      .then(res => setMessage(res.data))
-      .catch(err => console.error(err));
-  }, []);
-
   return (
-    <div style={{ textAlign: "center", marginTop: "100px", fontSize: "24px" }}>
-      {message}
+    <div>
+      <h1>Selam Can 👑</h1>
     </div>
   );
 }
